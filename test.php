7ef1ccbbdbdff250f0f00818a22ac8608a4617f0
Camilapbonassio <camilabonassio@gmail.com>
2017-12-19 23:50:06 -0200

    mud

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+	$element_1 = $_POST["element_1"];
+	$element_2 = $_POST["element_2"];
+	$element_3 = $_POST["element_3"];
+	
+	
+	$to = "camilabonassio@gmail.com";
+	$subject = "balaio_de_organicos";
+	$body = '<html>
+			<body>
+	
+			<p>nome:.'$element_1'.</p>
+			<p>celular:.'$element_2'</p>
+			<p>telefone:.'$element_3'</p>
+			</body>
+			</html>';
+	$send mail ($to,$subject, $body);
+	if ($send) {
+		echo '<br>';
+		echo "thanks";
+	}else {
+		echo "error";
+	}
+	}
+	
+?>
